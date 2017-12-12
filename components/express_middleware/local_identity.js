8bdd86de9063460df9fd80252466e6a17f6e5a89
clint <ccruson@zoho.com>
2017-12-12 16:20:00 -0600

    cleaning up local identity middleware

--- components/express_middleware/local_identity.js
+++ components/express_middleware/local_identity.js
@@ -1,9 +1,7 @@
 var request = require('request');
 
-
 module.exports = function(webserver, controller){
   // check static.local for identity
-  // console.log('webserver.locals: ', webserver.locals);
   if(!webserver.local || !webserver.local.local_identity){
     // if not call whoami and set it
     request({
@@ -11,29 +9,26 @@ module.exports = function(webserver, controller){
         uri: (controller.config.studio_command_uri || 'https://studio.botkit.ai') + '/api/v2/users/whoami/' + controller.config.studio_token
     },function(err, res, body) {
         if (err) {
-            console.log('Error registering instance with Botkit Studio', err);
+            console.log('Error confirming instance with Botkit Studio', err);
         } else {
             var json =null;
             try {
                 json = JSON.parse(body);
             } catch(err) {
-                console.log('Error registering instance with Botkit Studio', err);
+                console.log('Error confirming instance with Botkit Studio', err);
             }
 
             if (json) {
                 if (json.error) {
-                    console.log('Error registering instance with Botkit Studio', json.error);
+                    console.log('Error confirming instance with Botkit Studio', json.error);
                 }
-                // console.log('whoami: ', json.data);
-                webserver.locals.local_identity = json.data;
-                console.log('local_identity set from whoami...');
-                // console.log('webserver.locals.local_identity: ', webserver.locals.local_identity);
+                webserver.locals.bot_id = json.data.id;
+                controller.config.botkit_studio_id = json.data.id;
+                console.log('local_identity set from whoami... id:', webserver.locals.bot_id);
+
             }
         }
 
     });
   }
-
-
-
 }
