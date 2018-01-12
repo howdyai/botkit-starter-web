1cadb18d91223483f7e143c3beb370bb925f5cb6
Ben Brown <ben@xoxco.com>
2018-01-12 12:38:40 -0600

    point to different script names

--- skills/connection_events.js
+++ skills/connection_events.js
@@ -4,18 +4,18 @@ module.exports = function(controller) {
     controller.on('hello', function(bot, message) {
 
           // a new session with an unknown user has begun
-          controller.studio.run(bot, 'tutorial_hello', message.user, message.channel, message);
+          controller.studio.run(bot, 'welcome_user', message.user, message.channel, message);
     });
 
     controller.on('welcome_back', function(bot, message) {
 
         // a known user has started a new, fresh session
-        controller.studio.run(bot, 'tutorial_hello', message.user, message.channel, message).then(function(convo) {
+        controller.studio.run(bot, 'welcome_user', message.user, message.channel, message).then(function(convo) {
             convo.gotoThread('welcome_back');
         })
     });
 
-    controller.studio.before('tutorial_hello', function(convo, next) {
+    controller.studio.before('welcome_user', function(convo, next) {
         convo.setVar('bot', controller.studio_identity);
         next();
     });
