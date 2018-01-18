640fcfb1e7d9683d6d8e29403063cc4c7efaa0cb
Ben Brown <ben@xoxco.com>
2018-01-18 12:49:44 -0600

    disable by default the super quit

--- skills/super_quit.js
+++ skills/super_quit.js
@@ -1,30 +1,32 @@
 /*
 
+THIS MODULE IS DISABLED BY DEFAULT!
+
+UNCOMMENT THE CODE BELOW TO ACTIVATE THIS FEATURE
+
 This module causes your bot to listen for variations on the word "Quit"
 and causes it to immediately quit any ongoing conversation.
 */
 
 module.exports = function(controller) {
 
-    controller.middleware.receive.use(function(bot, message, next) {
-
-        if (message.text && message.text.match(bot.utterances.quit)) {
-            bot.findConversation(message, function(convo) {
-                if (convo) {
-                    // stop the conversation and swallow this message
-                    convo.stop('quit');
-                    bot.reply(message,'Quitting.');
-                } else {
-                    // nothing ongoing, this message passes through
-                    next();
-                }
-            });
-        } else {
-            next();
-        }
-
-    });
-
-
+    // controller.middleware.receive.use(function(bot, message, next) {
+    //
+    //     if (message.text && message.text.match(bot.utterances.quit)) {
+    //         bot.findConversation(message, function(convo) {
+    //             if (convo) {
+    //                 // stop the conversation and swallow this message
+    //                 convo.stop('quit');
+    //                 bot.reply(message,'Quitting.');
+    //             } else {
+    //                 // nothing ongoing, this message passes through
+    //                 next();
+    //             }
+    //         });
+    //     } else {
+    //         next();
+    //     }
+    //
+    // });
 
 }
